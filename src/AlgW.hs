b784699147f940495bb609fb987072444f069804
Nick Chapman <nic@amadido.co.uk>
2026-07-13 08:51:33 +0100

    split out Ctx module

--- src/AlgW.hs
+++ src/AlgW.hs
@@ -2,11 +2,10 @@ module AlgW
   ( typeOfExp
   ) where
 
-import AST (Exp,Id,mkUserId)
+import AST (Exp)
 import AST qualified (Exp(..),Bid(..),Literal(..))
-import Data.Map (Map)
-import Data.Map qualified as Map
-import Infer (IType,Infer(..),typeBase0,tuple,unify,(-->),getRefine2,ITypeScheme,generalize,instantiate,mono)
+import Ctx (Ctx,typeChar,typeString,typeInt,lookupCtx,insertCtx,ctx0,typesFromCtx)
+import Infer(Infer(..),IType,instantiate,unify,(-->),getRefine2,mono,generalize,tuple)
 import Pretty (Pretty(..))
 import TypeF (TypeScheme)
 
@@ -19,11 +18,9 @@ typeOfExp exp = do
 typeExp :: Ctx -> Exp -> Infer IType
 typeExp ctx exp = case exp of
   AST.Lam _pos (AST.Bid _ x) body -> do
-    let Ctx{xmap} = ctx
     arg <- IFresh
     IDebug $ "fresh(" <> pretty x <> "): -> " <> pretty arg
-    let ctx1 = ctx { xmap = Map.insert x (mono arg) xmap }
-    ret <- typeExp ctx1 body
+    ret <- typeExp (insertCtx ctx x (mono arg)) body
     pure $ (arg --> ret)
   AST.App e1 _pos e2 -> do
     ret <- IFresh
@@ -33,10 +30,7 @@ typeExp ctx exp = case exp of
     unify fun (arg --> ret)
     pure ret
   AST.Var _pos x -> do
-    let Ctx{xmap} = ctx
-    let err = error ("unbound var: '" <> pretty x <> "'")
-    let scheme = maybe err id $ Map.lookup x xmap
-    instantiate scheme
+    instantiate (lookupCtx ctx x)
   AST.Lit _pos  lit ->
     case lit of
       AST.LitN{} -> pure typeInt
@@ -45,36 +39,8 @@ typeExp ctx exp = case exp of
   AST.RecLam{} -> undefined
   AST.Let _p1 (AST.Bid _p2 x) eRhs eBody -> do
     rhs <- typeExp ctx eRhs
-    let Ctx{xmap} = ctx
-    let ss :: [ITypeScheme] = [ s | (_,s) <- Map.toList xmap ]
-    tScheme <- generalize ss rhs
-    let ctx1 = ctx { xmap = Map.insert x tScheme xmap }
-    typeExp ctx1 eBody
+    scheme <- generalize (typesFromCtx ctx) rhs
+    typeExp (insertCtx ctx x scheme) eBody
   AST.Tuple es -> do
     ts <- mapM (typeExp ctx) es
     pure $ tuple ts
-
-
-data Ctx = Ctx { xmap :: Map Id ITypeScheme }
-instance Pretty Ctx where pretty Ctx{xmap=m} = pretty m
-
-ctx0 :: Ctx
-ctx0 = Ctx { xmap = Map.fromList [ (mkUserId x, mono ty) | (x,ty) <- init ] }
-  where
-    init =
-      [ ("true", typeBool)
-      , ("false", typeBool)
-      , ("-", typeInt --> typeInt --> typeInt)
-      , ("+", typeInt --> typeInt --> typeInt)
-      , ("*", typeInt --> typeInt --> typeInt)
-      , ("/", typeInt --> typeInt --> typeInt)
-      , ("&&", typeBool --> typeBool --> typeBool)
-      , ("||", typeBool --> typeBool --> typeBool)
-      , ("not", typeBool --> typeBool)
-      ]
-
-typeInt,typeChar,typeString,typeBool :: IType
-typeInt = typeBase0 "Int"
-typeChar = typeBase0 "Char"
-typeString = typeBase0 "String"
-typeBool = typeBase0 "Bool"
---
+++ src/Ctx.hs
@@ -0,0 +1,49 @@
+module Ctx
+  ( Ctx
+  , ctx0
+  , typeChar,typeString,typeInt
+  , lookupCtx, insertCtx
+  , typesFromCtx
+  )
+  where
+
+import AST (Id,mkUserId)
+import Data.Map (Map)
+import Data.Map qualified as Map
+import Infer (IType,ITypeScheme,typeBase0,(-->),mono)
+import Pretty (Pretty(..))
+
+data Ctx = Ctx { xmap :: Map Id ITypeScheme }
+instance Pretty Ctx where pretty Ctx{xmap=m} = pretty m
+
+lookupCtx :: Ctx -> Id -> ITypeScheme
+lookupCtx Ctx{xmap} x =
+  maybe err id $ Map.lookup x xmap
+  where err = error ("unbound var: '" <> pretty x <> "'")
+
+insertCtx :: Ctx -> Id -> ITypeScheme -> Ctx
+insertCtx ctx@Ctx{xmap} x v = ctx { xmap = Map.insert x v xmap }
+
+typesFromCtx :: Ctx -> [ITypeScheme]
+typesFromCtx Ctx{xmap} = [ s | (_,s) <- Map.toList xmap ]
+
+ctx0 :: Ctx
+ctx0 = Ctx { xmap = Map.fromList [ (mkUserId x, mono ty) | (x,ty) <- init ] }
+  where
+    init =
+      [ ("true", typeBool)
+      , ("false", typeBool)
+      , ("-", typeInt --> typeInt --> typeInt)
+      , ("+", typeInt --> typeInt --> typeInt)
+      , ("*", typeInt --> typeInt --> typeInt)
+      , ("/", typeInt --> typeInt --> typeInt)
+      , ("&&", typeBool --> typeBool --> typeBool)
+      , ("||", typeBool --> typeBool --> typeBool)
+      , ("not", typeBool --> typeBool)
+      ]
+
+typeInt,typeChar,typeString,typeBool :: IType
+typeInt = typeBase0 "Int"
+typeChar = typeBase0 "Char"
+typeString = typeBase0 "String"
+typeBool = typeBase0 "Bool"
